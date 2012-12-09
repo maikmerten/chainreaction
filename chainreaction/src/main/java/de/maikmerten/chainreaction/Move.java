4c0313e1e31cda9b34be064c87137bca12ba138e
Johannes Neubauer <johannes.neubauer@cs.tu-dortmund.de>
2012-12-09 22:23:51 +0100

    add missing class

---
+++ chainreaction/src/main/java/de/maikmerten/chainreaction/Move.java
@@ -0,0 +1,34 @@
+package de.maikmerten.chainreaction;
+
+
+/**
+ * @author jonny
+ *
+ */
+public class Move {
+	private final int x1, y1, x2, y2;
+
+	public Move(int x1, int y1, int x2, int y2) {
+		super();
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+	}
+	
+	public int getX1() {
+		return x1;
+	}
+	
+	public int getY1() {
+		return y1;
+	}
+
+	public int getX2() {
+		return x2;
+	}
+	
+	public int getY2() {
+		return y2;
+	}
+}
