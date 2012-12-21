56914d3a5aa15ceb320291954669d107b35f62a7
Johannes Neubauer <johannes.neubauer@cs.tu-dortmund.de>
2012-12-21 19:49:48 +0100

    delayed animation

--- chainreaction/src/main/java/de/maikmerten/chainreaction/swing/SwingFieldListener.java
+++ chainreaction/src/main/java/de/maikmerten/chainreaction/swing/SwingFieldListener.java
@@ -1,6 +1,8 @@
 package de.maikmerten.chainreaction.swing;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.swing.SwingUtilities;
 
@@ -14,74 +16,67 @@ import de.maikmerten.chainreaction.Move;
  */
 public class SwingFieldListener implements FieldListener {
 	private FieldListener listener;
-
-	private int counter = 0;
-	private int swingCounter = 0;
+	private ExecutorService execService;
 
 	public SwingFieldListener(FieldListener listener) {
 		this.listener = listener;
+		this.execService = Executors.newSingleThreadExecutor();
 	}
 
 	@Override
 	public void onAtomAdded(final byte player, final int x, final int y) {
-		final int localCounter = ++counter;
-		SwingUtilities.invokeLater(new Runnable() {
-
+		executeDelayed(new Runnable() {
 			@Override
 			public void run() {
-				swingCounter++;
-				if(localCounter != swingCounter) {
-					throw new IllegalStateException(localCounter + ", " + swingCounter);
-				}
 				listener.onAtomAdded(player, x, y);
 			}
-		});
+
+		}, 40);
 	}
 
 	@Override
 	public void onAtomsMoved(final List<Move> moves) {
-		final int localCounter = ++counter;
-		SwingUtilities.invokeLater(new Runnable() {
-
+		executeDelayed(new Runnable() {
 			@Override
 			public void run() {
-				swingCounter++;
-				if(localCounter != swingCounter) {
-					throw new IllegalStateException(localCounter + ", " + swingCounter);
-				}
 				listener.onAtomsMoved(moves);
 			}
-		});
+		}, 40);
 	}
 
 	@Override
 	public void onOwnerChanged(final byte player, final int x, final int y) {
-		final int localCounter = ++counter;
-		SwingUtilities.invokeLater(new Runnable() {
-
+		executeDelayed(new Runnable() {
 			@Override
 			public void run() {
-				swingCounter++;
-				if(localCounter != swingCounter) {
-					throw new IllegalStateException(localCounter + ", " + swingCounter);
-				}
 				listener.onOwnerChanged(player, x, y);
 			}
-		});
+		}, 0);
 	}
 
 	@Override
 	public void onCellCleared(final int x, final int y) {
-		final int localCounter = ++counter;
-		SwingUtilities.invokeLater(new Runnable() {
-
+		executeDelayed(new Runnable() {
 			@Override
 			public void run() {
-				swingCounter++;
-				if(localCounter != swingCounter) {
-					throw new IllegalStateException(localCounter + ", " + swingCounter);
-				}
 				listener.onCellCleared(x, y);
+			}
+		}, 40);
+	}
+	
+	private void executeDelayed(final Runnable run, final long delay) {
+		execService.submit(new Runnable() {
+			@Override
+			public void run() {
+				SwingUtilities.invokeLater(run);
+				if(delay > 0) {
+					try {
+						Thread.sleep(delay);
+					}
+					catch (InterruptedException e) {
+						// do nothing
+					}
+				}
 			}
 		});
 	}
