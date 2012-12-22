33e46b348f342327595757801f8ad237cc2f5efd
Stephan Windmüller <windy@white-hawk.de>
2012-12-23 00:36:04 +0100

    Move settings to loader class

---
+++ chainreaction/src/main/java/de/maikmerten/chainreaction/SettingsLoader.java
@@ -0,0 +1,92 @@
+package de.maikmerten.chainreaction;
+
+import de.maikmerten.chainreaction.exceptions.ConfigUnreadableException;
+
+import java.io.*;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
+
+public class SettingsLoader {
+
+	/**
+	 * Loads the settings stored in the user's home directory.
+	 * If there are no settings, the default values are stores.
+	 *
+	 * @return
+	 *  	the loaded settings
+	 *
+	 * @throws
+	 *  	ConfigUnreadableException if there was an error while reading the settings
+	 */
+	public static Settings loadSettings() {
+		Properties properties = new Properties();
+		try {
+			checkConfigReadability();
+			String configFileName = getConfigurationFileLocation().toString();
+			BufferedInputStream stream = new BufferedInputStream(new FileInputStream(configFileName));
+			properties.load(stream);
+			int delay = Integer.valueOf(properties.getProperty("delay"));
+			return new Settings(delay);
+		} catch (ConfigUnreadableException e) {
+			Settings settingsWithDefaultValues = new Settings();
+			storeSettings(settingsWithDefaultValues);
+			return settingsWithDefaultValues;
+		} catch (IOException e) {
+			System.err.println("Unable to load properties: " + e.getMessage());
+			return new Settings();
+		}
+	}
+
+	private static void storeSettings(Settings settings) {
+		String configFileName = getConfigurationFileLocation().toString();
+		Properties properties = new Properties();
+		properties.setProperty("delay", String.valueOf(settings.getDelay()));
+		try {
+			BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(configFileName));
+			properties.store(stream, "Settings for Chain Reaction");
+		} catch (IOException | ConfigUnreadableException e) {
+			System.err.println("Unable to store properties: " + e.getMessage());
+		}
+	}
+
+	private static Path getConfigurationStorageLocation() {
+		String homeDir = System.getProperties().getProperty("user.home");
+		String configDir = ".chainReaction";
+		return FileSystems.getDefault().getPath(homeDir, configDir);
+	}
+
+	private static Path getConfigurationFileLocation() {
+		String filename = "settings.properties";
+		return FileSystems.getDefault().getPath(getConfigurationStorageLocation().toString(), filename);
+	}
+
+	private static void checkConfigReadability() {
+		Path path = getConfigurationStorageLocation();
+
+		if (!Files.exists(path)) {
+			try {
+				Files.createDirectory(path);
+			} catch (IOException e) {
+				throw new ConfigUnreadableException(e);
+			}
+		}
+		else if (!Files.isDirectory(path)) {
+			System.err.println("Unable to read settings, " + path + " is not a directory!");
+			throw new ConfigUnreadableException();
+		}
+
+		path = getConfigurationFileLocation();
+
+		if (!Files.exists(path)) {
+			throw new ConfigUnreadableException();
+		}
+
+		if (!Files.isRegularFile(path)) {
+			System.err.println("Unable to read settings from " + path + ", is not a regular file");
+			throw new ConfigUnreadableException();
+		}
+	}
+
+}
--- chainreaction/src/main/java/de/maikmerten/chainreaction/swing/UIGame.java
+++ chainreaction/src/main/java/de/maikmerten/chainreaction/swing/UIGame.java
@@ -1,21 +1,12 @@
 package de.maikmerten.chainreaction.swing;
 
-import de.maikmerten.chainreaction.Game;
-import de.maikmerten.chainreaction.MoveListener;
-import de.maikmerten.chainreaction.Player;
-import de.maikmerten.chainreaction.Settings;
+import de.maikmerten.chainreaction.*;
 import de.maikmerten.chainreaction.ai.AI;
 import de.maikmerten.chainreaction.ai.AIThread;
 import de.maikmerten.chainreaction.ai.StandardAI;
-import de.maikmerten.chainreaction.exceptions.ConfigUnreadableException;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.*;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -38,79 +29,10 @@ public class UIGame extends JFrame implements MoveListener {
 	private UIField uifield;
 
 	public UIGame() {
-		Settings settings = loadSettings();
+		Settings settings = SettingsLoader.loadSettings();
 		initGUI();
 		startNewGame();
 		setVisible(true);
-	}
-
-	private Settings loadSettings() {
-		Properties properties = new Properties();
-		try {
-			checkConfigReadability();
-			String configFileName = getConfigurationFileLocation().toString();
-			BufferedInputStream stream = new BufferedInputStream(new FileInputStream(configFileName));
-			properties.load(stream);
-			int delay = Integer.valueOf(properties.getProperty("delay"));
-			return new Settings(delay);
-		} catch (ConfigUnreadableException e) {
-			Settings settingsWithDefaultValues = new Settings();
-			storeSettings(settingsWithDefaultValues);
-			return settingsWithDefaultValues;
-		} catch (IOException e) {
-			System.err.println("Unable to load properties: " + e.getMessage());
-			return new Settings();
-		}
-	}
-
-	private void storeSettings(Settings settings) {
-		String configFileName = getConfigurationFileLocation().toString();
-		Properties properties = new Properties();
-		properties.setProperty("delay", String.valueOf(settings.getDelay()));
-		try {
-			BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(configFileName));
-			properties.store(stream, "Settings for Chain Reaction");
-		} catch (IOException | ConfigUnreadableException e) {
-			System.err.println("Unable to store properties: " + e.getMessage());
-		}
-	}
-
-	private Path getConfigurationStorageLocation() {
-		String homeDir = System.getProperties().getProperty("user.home");
-		String configDir = ".chainReaction";
-		return FileSystems.getDefault().getPath(homeDir, configDir);
-	}
-
-	private Path getConfigurationFileLocation() {
-		String filename = "settings.properties";
-		return FileSystems.getDefault().getPath(getConfigurationStorageLocation().toString(), filename);
-	}
-
-	private void checkConfigReadability() {
-		Path path = getConfigurationStorageLocation();
-
-		if (!Files.exists(path)) {
-			try {
-				Files.createDirectory(path);
-			} catch (IOException e) {
-				throw new ConfigUnreadableException(e);
-			}
-		}
-		else if (!Files.isDirectory(path)) {
-			System.err.println("Unable to read settings, " + path + " is not a directory!");
-			throw new ConfigUnreadableException();
-		}
-
-		path = getConfigurationFileLocation();
-
-		if (!Files.exists(path)) {
-			throw new ConfigUnreadableException();
-		}
-
-		if (!Files.isRegularFile(path)) {
-			System.err.println("Unable to read settings from " + path + ", is not a regular file");
-			throw new ConfigUnreadableException();
-		}
 	}
 
 	private void initGUI() {
