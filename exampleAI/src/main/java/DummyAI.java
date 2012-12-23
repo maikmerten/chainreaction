fbab67b81b00261116be4928ee346a8c0bd9eeee
Stephan Windmüller <windy@white-hawk.de>
2012-12-23 10:23:01 +0100

    New DummyAI project

---
+++ exampleAI/src/main/java/DummyAI.java
@@ -0,0 +1,13 @@
+import de.maikmerten.chainreaction.ai.AI;
+
+public class DummyAI implements AI {
+
+	@Override
+	public void doMove() {
+	}
+
+	@Override
+	public String getName() {
+		return "Dummy AI";
+	}
+}
