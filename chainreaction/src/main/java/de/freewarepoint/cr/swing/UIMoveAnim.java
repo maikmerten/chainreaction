ae5b2924efd6791fdb96da23cba7f1fdea8926b1
Johannes Neubauer <johannes.neubauer@cs.tu-dortmund.de>
2013-01-07 21:16:06 +0100

    cleanup

--- chainreaction/src/main/java/de/freewarepoint/cr/swing/UIMoveAnim.java
+++ chainreaction/src/main/java/de/freewarepoint/cr/swing/UIMoveAnim.java
@@ -9,13 +9,11 @@ import static de.freewarepoint.cr.swing.UIField.CELL_SIZE;
 
 public class UIMoveAnim implements UIAnimation {
 	private static final int DELAY = 25;
-	private final int animCount = 25, brightness = 125, fadeCount = 20;
+	private final int animCount = 25, brightness = 125;
 
-	private int animCounter = 0, fadeCounter = 0;
+	private int animCounter = 0;
 	private long lastAnim = System.currentTimeMillis();
-	private boolean raise = true;
-	private Player player;
-	private Player oldPlayer;
+	private final Player player;
 	private final int x, y;
 
 	public UIMoveAnim(final int x, final int y, final Player player) {
@@ -29,32 +27,11 @@ public class UIMoveAnim implements UIAnimation {
 		final AffineTransform transform = g2d.getTransform();
 		g2d.translate((x * CELL_SIZE * 2), (y * CELL_SIZE * 2));
 		final long now = System.currentTimeMillis();
-		Color color;
-		if(oldPlayer != null && fadeCounter == fadeCount) {
-			oldPlayer = null;
-		}
-		if(oldPlayer != null) {
-			final Color oldColor = UIPlayer.getPlayer(oldPlayer).getBackground();
-			final Color newColor = UIPlayer.getPlayer(player).getBackground();
-			color = new Color(
-					calcCurrFadeColor(oldColor.getRed(), newColor.getRed()),
-					calcCurrFadeColor(oldColor.getGreen(), newColor.getGreen()),
-					calcCurrFadeColor(oldColor.getBlue(), newColor.getBlue()));
-		}
-		else {
-			color = UIPlayer.getPlayer(player).getBackground();
-		}
-
+		final Color color = UIPlayer.getPlayer(player).getBackground();
 		
 		if((now - lastAnim) > DELAY) {
 			final int anims = (int)(now - lastAnim)/DELAY;
-			if(oldPlayer != null) {
-				fadeCounter = (fadeCounter + anims);
-				if(fadeCounter > fadeCount) {
-					fadeCounter = fadeCount;
-				}
-			}
-			animCounter = (animCounter + (raise ? anims : - anims));
+			animCounter += anims;
 			if(animCounter > animCount) {
 				animCounter = animCount;
 			}
@@ -75,11 +52,6 @@ public class UIMoveAnim implements UIAnimation {
 		
 		g2d.setColor(oldColor);
 		g2d.setTransform(transform);
-	}
-	
-	private int calcCurrFadeColor(final int oldCanal, final int newCanal) {
-		return oldCanal + (fadeCounter * (newCanal - oldCanal)) / (fadeCount-1);
-		
 	}
 
 	@Override
